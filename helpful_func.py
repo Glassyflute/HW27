faa048d7f257688e5a2e1d99fb22a18884514fdd
Glassyflute <yuliya.bobir@gmail.com>
2022-12-11 13:13:42 +0600

    updated functions to load json into fixtures for loaddata

--- helpful_func.py
+++ helpful_func.py
@@ -1,88 +1,39 @@
 import json
 
 
-def ads_json_restructured(json_file, model):
+def json_restructured(json_file, model):
 
     with open(json_file, "r", encoding="utf-8") as json_f:
         data = json.load(json_f)
 
         result_list = []
-        for element in data:
+
+        for dict_element in data:
+            fields_item = dict(dict_element)
+            del fields_item["id"]
             result_list.append(
                 {
                     "model": model,
-                    "pk": element["id"],
-                    "fields": {
-                        "name": element["name"],
-                        "author": element["author"],
-                        "price": element["price"],
-                        "description": element["description"],
-                        "address": element["address"],
-                        "is_published": element["is_published"]
-                    }
+                    "pk": dict_element["id"],
+                    "fields": fields_item
                 }
             )
 
         return result_list
 
 
-def categories_json_restructured(json_file, model):
-
-    with open(json_file, "r", encoding="utf-8") as json_f:
-        data = json.load(json_f)
-
-        result_list = []
-        for element in data:
-            result_list.append(
-                {
-                    "model": model,
-                    "pk": element["id"],
-                    "fields": {
-                        "name": element["name"]
-                    }
-                }
-            )
-
-        return result_list
-
-
-# def json_restructured(json_file, model):
-#
-#     with open(json_file, "r", encoding="utf-8") as json_f:
-#         data = json.load(json_f)
-#
-#         result_list = []
-#
-#         fields_dict = []
-#
-#         for element in data:
-#             for key in element.keys():
-#
-#
-#             result_list.append(
-#                 {
-#                     "model": model,
-#                     "pk": element["id"],
-#                     "fields": {
-#                         "name": element["name"]
-#                     }
-#                 }
-#             )
-#
-#         return result_list
-
-
-def data_to_json_file_ads(json_file, json_newfile, model):
+def data_to_json_file(json_file, json_newfile, model):
     with open(json_newfile, "w", encoding="utf-8") as json_f_new:
-        data = ads_json_restructured(json_file, model)
+        data = json_restructured(json_file, model)
         json_f_new.write(json.dumps(data, ensure_ascii=False))
 
 
-def data_to_json_file_cat(json_file, json_newfile, model):
-    with open(json_newfile, "w", encoding="utf-8") as json_f_new:
-        data = categories_json_restructured(json_file, model)
-        json_f_new.write(json.dumps(data, ensure_ascii=False))
+a = json_restructured("ads_wn.json", "ads.ad")
+b = json_restructured("categories.json", "ads.category")
+print(a)
+print(b)
 
 
-output = data_to_json_file_ads("ads_wn.json", "ads_wn_fixt.json", "ads.ad")
-output2 = data_to_json_file_cat("categories.json", "categories_fixt.json", "ads.category")
+output3 = data_to_json_file("ads_wn.json", "ads_fixt.json", "ads.ad")
+output4 = data_to_json_file("categories.json", "categories_fixt.json", "ads.category")
+
